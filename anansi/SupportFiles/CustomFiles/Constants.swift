d330f8ef4be61dfcf6408c5870cec6b156483571
JNGApura <joao.apura@gmail.com>
2018-01-15 00:49:53 -0800

    Some cleaning and custom classes

--- anansi/SupportFiles/CustomFiles/Constants.swift
+++ anansi/SupportFiles/CustomFiles/Constants.swift
@@ -10,7 +10,7 @@ import UIKit
 
 enum Color {
 
-    static let primary = UIColor(red: 230/255.0, green: 43/255.0, blue: 30/255.0, alpha: 1.0)
+    static let primary = UIColor.red //UIColor(red: 230/255.0, green: 43/255.0, blue: 30/255.0, alpha: 1.0)
     static let secondary = UIColor.black
     static let tertiary = UIColor(red: 235/255.0, green: 235/255.0, blue: 241/255.0, alpha: 1.0)
     static let background = UIColor.white
---
+++ anansi/SupportFiles/CustomFiles/CustomClasses.swift
@@ -0,0 +1,40 @@
+//
+//  CustomClasses.swift
+//  anansi
+//
+//  Created by João Nuno Gaspar Apura on 15/01/2018.
+//  Copyright © 2018 João Apura. All rights reserved.
+//
+
+import UIKit
+
+// Class to add KeyBoardAcessoryToolbar on top of UIToolbar with customizable buttons
+class KeyboardAccessoryToolbar: UIToolbar {
+    
+    convenience init() {
+        self.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        self.barStyle = .default
+        self.isTranslucent = true
+        self.tintColor = Color.primary
+        self.backgroundColor = Color.tertiary
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        //let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        //self.items = [cancelButton, spaceButton, doneButton]
+        self.items = [spaceButton, doneButton]
+        
+        self.isUserInteractionEnabled = true
+        self.sizeToFit()
+    }
+    
+    @objc func done() {
+        // Tell the current first responder (the current text input) to resign.
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    @objc func cancel() {
+        // Call "cancel" method on first object in the responder chain that implements it.
+        UIApplication.shared.sendAction(#selector(cancel), to: nil, from: nil, for: nil)
+    }
+}

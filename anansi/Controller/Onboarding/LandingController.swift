7532d79ea778261f3478bce342f683123726a523
JNGApura <joao.apura@gmail.com>
2019-04-12 00:49:04 +0100

    LandingController refactor. Removed textfield dependency and added new copy.

--- anansi/Controller/Onboarding/LandingController.swift
+++ anansi/Controller/Onboarding/LandingController.swift
@@ -13,51 +13,50 @@ class LandingController: UIViewController {
     
     // Custom initializers
     let logo: UIImageView = {
-        let i = UIImageView(image: #imageLiteral(resourceName: "TEDxISTAlameda-black"))
+        let i = UIImageView(image: UIImage(named: "TEDxULisboa-black"))
         i.translatesAutoresizingMaskIntoConstraints = false
         i.contentMode = .scaleAspectFill
         return i
     }()
     
-    let supportButton: TertiaryButton = {
+    let pageTitle: UILabel = {
+        let t = UILabel()
+        t.text = "We're very excited you are here!"
+        t.formatTextWithLineSpacing(lineSpacing: 10, lineHeightMultiple: 1.2, hyphenation: 0, alignment: .left)
+        t.textColor = .primary
+        t.font = UIFont.boldSystemFont(ofSize: Const.bodyFontSize)
+        t.lineBreakMode = .byWordWrapping
+        t.numberOfLines = 0
+        t.translatesAutoresizingMaskIntoConstraints = false
+        return t
+    }()
+    
+    let pageDescription: UILabel = {
+        let t = UILabel()
+        t.text = "This app is exclusive for TEDxULisboa 2019 attendees. Please make sure you have a ticket."
+        t.formatTextWithLineSpacing(lineSpacing: 10, lineHeightMultiple: 1.2, hyphenation: 0, alignment: .left)
+        t.textColor = .secondary
+        t.font = UIFont.systemFont(ofSize: Const.bodyFontSize)
+        t.lineBreakMode = .byWordWrapping
+        t.numberOfLines = 0
+        t.translatesAutoresizingMaskIntoConstraints = false
+        return t
+    }()
+    
+    let wantsTicketButton: TertiaryButton = {
         let b = TertiaryButton()
-        b.setTitle("Need help?", for: .normal)
+        b.setTitle("How do I get a ticket?", for: .normal)
         b.setTitleColor(.secondary, for: .normal)
-        b.backgroundColor = .clear
-        b.addTarget(self, action: #selector(sendToMessenger), for: .touchUpInside)
+        b.addTarget(self, action: #selector(sendToFever), for: .touchUpInside)
         b.translatesAutoresizingMaskIntoConstraints = false
         return b
     }()
     
-    let descriptionText = "<style>*{font-family:\"Avenir-Roman\"; font-size: 17px}</style><p>This app is currently only available to attendees of TEDxISTAlameda 2018 conference. If you are looking for TED on your mobile device, check out <a href=\"https://itunes.apple.com/us/app/ted/id376183339\">this app</a>.</p></body>"
-
-    lazy var pageDescription: UITextView = {
-        let text = UITextView()
-        text.textContainerInset = UIEdgeInsetsMake(8.0, -4.0, 8.0, -4.0) // top, left, bottom, right
-        text.translatesAutoresizingMaskIntoConstraints = false
-        text.isScrollEnabled = false
-        text.isEditable = false
-        text.formatHTMLText(htmlText: descriptionText, lineSpacing: 10, lineHeightMultiple: 1.2, hyphenation: 0, alignment: .left)
-        return text
-    }()
-    
-    lazy var hasTicketReferenceButton: PrimaryButton = {
+    let hasTicketReferenceButton: PrimaryButton = {
         let b = PrimaryButton()
         b.setTitle("I have a ticket reference", for: .normal)
         b.translatesAutoresizingMaskIntoConstraints = false
-        b.alpha = 1
-        b.isEnabled = true
-        b.addTarget(self, action: #selector(sendUserToSignUpPage), for: .touchUpInside)
-        return b
-    }()
-    
-    lazy var wantsTicketButton: SecondaryButton = {
-        let b = SecondaryButton()
-        b.setTitle("How do I get a ticket?", for: .normal)
-        b.translatesAutoresizingMaskIntoConstraints = false
-        b.alpha = 1
-        b.isEnabled = true
-        b.addTarget(self, action: #selector(sendUserToGetTicket), for: .touchUpInside)
+        b.addTarget(self, action: #selector(toSignUp), for: .touchUpInside)
         return b
     }()
     
@@ -69,62 +68,39 @@ class LandingController: UIViewController {
         view.backgroundColor = .white
         
         // Set up constraint constants according to iphone height | use case: motherf*cker iphone5
-        let spacingTopLogo: CGFloat
-        let spacingLogoDescription: CGFloat
-        
         if Display.typeIsLike == .iphone5 {
-            spacingTopLogo = Const.marginAnchorsToContent * 2.0
-            spacingLogoDescription = Const.marginAnchorsToContent * 2.0
-            pageDescription.formatHTMLText(htmlText: descriptionText, lineSpacing: 8, lineHeightMultiple: 1.1, hyphenation: 0, alignment: .left)
-        
-        } else {
-            spacingTopLogo = Const.marginAnchorsToContent * 3.0
-            spacingLogoDescription = Const.marginAnchorsToContent * 4.0
+            pageTitle.formatTextWithLineSpacing(lineSpacing: 8, lineHeightMultiple: 1.1, hyphenation: 0, alignment: .left)
+            pageDescription.formatTextWithLineSpacing(lineSpacing: 8, lineHeightMultiple: 1.1, hyphenation: 0, alignment: .left)
         }
         
+        [logo, pageTitle, pageDescription, hasTicketReferenceButton, wantsTicketButton].forEach { view.addSubview($0) }
+        
         // Logo
-        view.addSubview(logo)
         NSLayoutConstraint.activate([
-            logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: spacingTopLogo),
-            logo.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Const.marginAnchorsToContent),
-        ])
-        
-        // Description text
-        view.addSubview(pageDescription)
-        NSLayoutConstraint.activate([
-            pageDescription.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: spacingLogoDescription),
+            logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Const.marginSafeArea * 2.0),
+            logo.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Const.marginSafeArea),
+            
+            pageTitle.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: Const.marginSafeArea * 2.0),
+            pageTitle.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            pageTitle.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -Const.marginSafeArea * 2.0),
+            
+            pageDescription.topAnchor.constraint(equalTo: pageTitle.bottomAnchor),
             pageDescription.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            pageDescription.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -Const.marginAnchorsToContent * 2.0),
-            //pageDescription.heightAnchor.constraint(equalToConstant: 205.0)
-        ])
-        
-        // hasTicketReferenceButton button
-        view.addSubview(hasTicketReferenceButton)
-        NSLayoutConstraint.activate([
-            hasTicketReferenceButton.topAnchor.constraint(equalTo: pageDescription.bottomAnchor, constant: Const.marginAnchorsToContent * 1.5),
+            pageDescription.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -Const.marginSafeArea * 2.0),
+            
+            wantsTicketButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Const.marginSafeArea),
+            wantsTicketButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            wantsTicketButton.heightAnchor.constraint(equalToConstant: Const.buttonHeight),
+            wantsTicketButton.widthAnchor.constraint(equalToConstant: 246.0),
+            
+            hasTicketReferenceButton.bottomAnchor.constraint(equalTo: wantsTicketButton.topAnchor, constant: -16.0),
             hasTicketReferenceButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             hasTicketReferenceButton.heightAnchor.constraint(equalToConstant: Const.buttonHeight),
             hasTicketReferenceButton.widthAnchor.constraint(equalToConstant: 246.0)
         ])
         
-        // wantsTicketButton button
-        view.addSubview(wantsTicketButton)
-        NSLayoutConstraint.activate([
-            wantsTicketButton.topAnchor.constraint(equalTo: hasTicketReferenceButton.bottomAnchor, constant: 16.0),
-            wantsTicketButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            wantsTicketButton.heightAnchor.constraint(equalToConstant: Const.buttonHeight),
-            wantsTicketButton.widthAnchor.constraint(equalToConstant: 246.0)
-        ])
-        
-        // Support button
-        view.addSubview(supportButton)
-        NSLayoutConstraint.activate([
-            supportButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Const.marginAnchorsToContent),
-            supportButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Const.marginAnchorsToContent),
-        ])
-        
         // Sets "isOnboarded" to true in UserDefaults
-        UserDefaults.standard.setOnboarded(value: true)
+        //UserDefaults.standard.setOnboarded(value: true)
     }
     
     override func didReceiveMemoryWarning() {
@@ -133,47 +109,21 @@ class LandingController: UIViewController {
     
     // MARK: Custom functions
     
-    // Send to messenger (support)
-    @objc func sendToMessenger() {
+    // Send to FeverUp website
+    @objc func sendToFever() {
         
-        let url = URL(string: "fb-messenger://user-thread/tedxistalameda")!
-        
-        UIApplication.shared.open(url, options: [:]) { (success) in
-            
-            if success == false {
-                
-                let url = URL(string: "https://m.me/tedxistalameda")
-                if UIApplication.shared.canOpenURL(url!) {
-                    UIApplication.shared.open(url!)
-                }
-            }
+        let url = URL(string: "https://www.feverup.com/m/72092/?ref=6405")
+        if UIApplication.shared.canOpenURL(url!) {
+            UIApplication.shared.open(url!)
         }
     }
     
     // Sends user to SignUpController with custom transition
-    @objc func sendUserToSignUpPage() {
-    
+    @objc func toSignUp() {
+        
         let controller = SignUpController()
         controller.modalPresentationStyle = .overFullScreen
         controller.modalTransitionStyle = .crossDissolve
         present(controller, animated: true, completion: nil)
     }
-    
-    // Sends user to get a ticket (outside app) // TO DO!
-    @objc func sendUserToGetTicket() {
-        
-        let url = URL(string: "fb-messenger://user-thread/tedxistalameda")!
-        
-        UIApplication.shared.open(url, options: [:]) { (success) in
-            
-            if success == false {
-                
-                let url = URL(string: "https://m.me/tedxistalameda")
-                if UIApplication.shared.canOpenURL(url!) {
-                    UIApplication.shared.open(url!)
-                }
-            }
-        }
-    }
-
 }
